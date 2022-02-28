4e9dda2d31758d85a05dd3df41d48533312ad315
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-02-28 16:50:31 +0100

    dynamic-version

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var allSelect = document.getElementsByClassName("first");
+var addButton = document.getElementById("button-new");
+var removeButton = document.getElementById("button-remove");
+var allSelect = document.querySelectorAll("select");
+var allInputs = document.querySelector("input.quantite");
+var allSelOptions = document.querySelectorAll("option");
 var allQut = document.getElementsByClassName("quantite");
-var allTLine = document.getElementsByClassName("totLine");
+var allTLine = document.getElementsByClassName("input-total-line");
 var toTotalCase = document.getElementById("toTotal");
-var inputSelect;
+var UI = document.getElementById("u-interface");
+var divInputs = document.getElementById(`all-inputs ${numLigne}`);
+var inputsLine = document.getElementsByClassName(`order-form-field ligne ${numLigne}`);
+var qutElement;
+var refElement;
+var puElement;
+var pElement;
+var numLigne = 0;
 var index = 0;
 var elementTotal = 0;
 var elementQuantite = 0;
@@ -12,144 +23,184 @@ var selectId;
 var sum = 0;
 var pTotalId = ""; 
 var infoArticle = [
-    [0, 1233, 3455, 5677, 7899, 1111, 6666],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 10, 20, 30, 2, 11, 15],
-    [0, 0, 0, 0, 0, 0, 0],
-];
+    [0, 0, 0, 0],
+    [1233, 0, 10, 0],
+    [3455, 0, 20, 0],
+    [5677, 0, 30, 0],
+    [7899, 0, 2, 0],
+    [1111, 0, 11, 0],
+    [6666, 0, 15, 0]
+]
+optionLibelles = [
+    "Choisissez votre livre",
+    "Le grand livre de Javascript",
+    "HTML encore plus vite",
+    "Windows NT4.0",
+    "Le Kit de Ressource technique de NT",
+    "Formation Java",
+    "Comment créer son site Web"
+]
 
+//Création des élements du formulaire
+function addElement()
+{  
+    numLigne++;
 
-function remplirForm()
-{
-    for (j=0; j<4; j++)
-    { 
-        inputSelect[j].value = infoArticle[j][index];
-    }
-}
+    //La div contenant le formulaire
+    var inputs = document.createElement("div");
+    inputs.id ="all-inputs" + numLigne;
+    inputs.setAttribute("class", "inputs");
 
-function calcTotalLigne()
-{
-    total = elementQuantite * elementPrixUnitaire;
-    elementTotal.value = total; 
-
-    return total;
-}
-
-function calcToTotal()
-{
-    for (k=0; k<allTLine.length; k++)
+    //Le select ainsi que les options qui s'y rattachent
+    var newSelect = document.createElement("select");
+    newSelect.id ="select-item" + numLigne;
+    newSelect.setAttribute("class", "select-element");
+    for (i=0; i<optionLibelles.length; i++) 
     {
-        if (allTLine[k].value !== "")
-        {
-            sum = parseInt(sum) + parseInt(allTLine[k].value);
-            toTotalCase.value = sum; 
-        }      
-    }
-    sum = 0;  
+        var Option = document.createElement("option");
+        Option.text = optionLibelles[i];
+        newSelect.add(Option, null);
+    }  
+    
+    //Les inputs relatifs à la réference
+    var inputRef = document.createElement("input");
+    inputRef.setAttribute("type", "text");
+    inputRef.id = "input-ref" + numLigne;
+    inputRef.setAttribute("disabled", "disabled");
+    inputRef.classList.add("order-form-field", "ligne"+numLigne);
+    //La quantité
+    var inputQut = document.createElement("input");
+    inputQut.setAttribute("type", "number");
+    inputQut.id = "input-qut" + numLigne;
+    inputQut.classList.add("order-form-field", "ligne"+numLigne);
+    inputQut.classList.add("quantite");
+    //Le prix unitaire
+    var inputPu = document.createElement("input");
+    inputPu.setAttribute("type", "number");
+    inputPu.id = "input-pu" + numLigne;
+    inputPu.classList.add("order-form-field", "ligne"+numLigne); 
+    inputPu.setAttribute("disabled", "disabled");
+    //Et le total par ligne
+    var inputP = document.createElement("input");
+    inputP.setAttribute("type", "number");
+    inputP.id = "input-p" + numLigne;
+    inputP.setAttribute("class", "input-total-line"+numLigne);
+    inputP.setAttribute("disabled", "disabled");
+
+    //Les différents élements crées sont insérés dans le DOM
+    UI.insertAdjacentElement("beforebegin", inputs);
+    inputs.insertAdjacentElement("afterbegin", newSelect); 
+    inputs.insertAdjacentElement("beforeend", inputRef);
+    inputs.insertAdjacentElement("beforeend", inputQut);
+    inputs.insertAdjacentElement("beforeend", inputPu);
+    inputs.insertAdjacentElement("beforeend", inputP);  
 }
 
-function reset()
-{
-    if (index == 0)
-    {
-        calcToTotal();
-    }
+
+//Création des events placés sur des selecteurs puis ciblés par le biais de l'event.target 
+function addEvent(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e)
+    })
 }
 
 //La ligne est remplie en fonction du bouton select utilisé. 
 //Si l'option par défaut est sélectionnée, la ligne est remise à zéro et le total recalculé. 
-for (i=0; i<allSelect.length; i++)
-{         
-    allSelect[i].addEventListener("change", function() 
-    {   
-        selectId = this.id;  
-        index =  this.options.selectedIndex;
-
-        var elementSelect = document. getElementById(selectId);
-        var trSelect = elementSelect.parentNode.parentNode;  
-        inputSelect = trSelect.getElementsByTagName("input");       
-
-        remplirForm();  
-
-        reset();
-    }) 
-}
-
-//Le total de la ligne est calculé et affiché lorsque la quantité est modifiée
-for (i=0; i<allQut.length; i++)
+addEvent("change", "select", e => 
 {
-    allQut[i].addEventListener("change", function()   
-    {   
-        var qutId = "";   
-        var pUnitId = "";
-        var pTotalId = "";
-        switch(this.id)
+    selectId = e.target.id;
+    numSelectId = selectId.substring(11, selectId.length); 
+    index =  e.target.options.selectedIndex;
+    
+    var refElement = document.getElementById(`input-ref${numSelectId}`);
+    var qutElement = document.getElementById(`input-qut${numSelectId}`);
+    var puElement = document.getElementById(`input-pu${numSelectId}`);
+    var pElement = document.getElementById(`input-p${numSelectId}`);
+
+    switch(e.target.id)
+    {
+        case selectId:
+            refElement.value = infoArticle[index][0];
+            qutElement.value = infoArticle[index][1];
+            puElement.value = infoArticle[index][2];
+           
+        break;  
+
+        default:
+            console.log("error");
+        break;
+    }
+ 
+    //Reset ligne
+    if (index == 0)
+    {
+        allTLine = document.querySelectorAll(`input.input-total-line${numSelectId}`);
+        for (k=0; k<allTLine.length; k++)
         {
-            case "qut1":
-                pUnitId = "pu1";
-                qutId = "qut1";
-                pTotalId ="p1";
-            break;
+            if (allTLine[k].value !== "")
+            {
+                pElement = document.getElementById(`input-p${numSelectId}`);
+                sum = parseInt(sum) - parseInt(pElement.value);
+                toTotalCase.value = sum; 
+            }      
+        }  
+        pElement = document.getElementById(`input-p${numSelectId}`);
+        pElement.value = infoArticle[index][3];  
 
-            case "qut2":
-                pUnitId = "pu2";
-                qutId = "qut2";
-                pTotalId ="p2";
-            break;
+        divInputs = document.getElementById(`all-inputs${numSelectId}`);
+        divInputs.remove();
+    }
+    else
+    {
+        e.target.options[e.target.selectedIndex].disabled = true;
+        optionLibelles.splice(index, 1);  
+        infoArticle.splice(index, 1);
+    }
+})
 
-            case "qut3":
-                pUnitId = "pu3";
-                qutId = "qut3";
-                pTotalId ="p3";
-            break;
 
-            case "qut4":
-                pUnitId = "pu4";
-                qutId = "qut4";
-                pTotalId ="p4";
-            break;
+//Le total par ligne est calculé lorsque la quantité est modifiée
+addEvent("change", "input.quantite", e =>
+{
+    var numQutId = e.target.id.substring(9, e.target.id.length); 
+    puElement = document.getElementById(`input-pu${numQutId}`);
+    pElement = document.getElementById(`input-p${numQutId}`);
+    var nbArticle = e.target.value;
+    var prixBase = puElement.value;
 
-            case "qut5":
-                pUnitId = "pu5";
-                qutId ="qut5";
-                pTotalId = "p5";
-            break;
-
-            case "qut6":
-                pUnitId = "pu6";
-                qutId = "qut6";
-                pTotalId = "p6";
-            break;
-
-            default:
-                pTotalId = "";
-            break;
-        }
-        elementTotal = document.getElementById(pTotalId);
-        elementQuantite = document.getElementById(qutId).value;
-        elementPrixUnitaire = document.getElementById(pUnitId).value;      
-        calcTotalLigne();      
-    });     
-}
+    total = nbArticle * prixBase;
+    pElement.value = total; 
+    
+    return total;
+}) 
 
 //L'input quantité est clear lors de la 1ère saisie
-for (i=0; i<allTLine.length; i++)
+addEvent("focusin", "input", e =>
 {
-    allQut[i].addEventListener("focusin", function(){
-        if (this.value == 0) 
-        { 
-            this.value = ''; 
-        }
-    });
-}
+    if (e.target.value == 0) 
+    { 
+        e.target.value = ''; 
+    }
 
-//Le total des totaux est calculé et affiché lorsque la saisie de la quantité est terminée
-for (i=0; i<allTLine.length; i++)
+})
+
+//Les totaux sont calculé lorsque la saisie de la quantité est terminée
+addEvent("focusout", "input", e =>
 {
-    allQut[i].addEventListener("focusout", function(){
-        calcToTotal();
-    });
-}
+    allTLine = document.querySelectorAll(`input.input-total-line${numLigne}`);
+    console.log(allTLine);
 
+    for (k=0; k<allTLine.length; k++)
+    {
+        if (allTLine[k].value !== "")
+        {
+            pElement = document.getElementById(`input-p${numLigne}`);
+            sum = parseInt(sum) + parseInt(pElement.value);
+            toTotalCase.value = sum; 
+        }      
+    }
 
+})
+
+addButton.addEventListener("click", addElement);
 
